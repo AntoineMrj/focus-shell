1be8aa6861cb448b295cf72de4539e12d21b3594
Marjault <antoine@DESKTOP-A3V8Q98.localdomain>
2018-03-23 22:16:24 +0100

    Validation temporaire de la liste chainé

--- interpreteur/linkedList.c
+++ interpreteur/linkedList.c
@@ -1,27 +1,31 @@
 #include "linkedList.h"
 
-void add(commandList *list,cmdIdentifier function)
+void add(commandList *list, cmdIdentifier function)
 {
-  commandListCell *newCell = (commandListCell*)malloc(sizeof(commandListCell)); //allocation de la mémoire pour la nouvelle cellule
+  commandListCell *newCell = (commandListCell *)malloc(sizeof(commandListCell)); //allocation de la mémoire pour la nouvelle cellule
 
-  if (newCell == NULL) {
+  if (newCell == NULL)
+  {
     printf("Allocation de mémoire impossible\n");
     exit(-1);
   }
 
-//creation de la Cellule
-newCell->function = function;
-newCell->next=NULL; //Pour l'instant pas de classement par ordre alphabétique donc on ajoute la cell a la fin
+  //creation de la Cellule
+  newCell->function = function;
+  newCell->next = NULL; //On ajoute la cellule à la fin
 
-if(list->head == NULL){ //si la liste et vide on met la cellule dans head
-  list->head = newCell;
-}
-else{ //sinon on cherche le dernier élément et on ajoute la cellule après
-  commandListCell *currentCell = list->head;
-  while(currentCell->next != NULL){ //on se place à la fin de la liste
-    currentCell = currentCell->next;
+  if (list->head == NULL)
+  { //si la liste et vide on met la cellule dans head
+    list->head = newCell;
+  }
+  else
+  { //sinon on cherche le dernier élément et on ajoute la cellule après
+    commandListCell *currentCell = list->head;
+    while (currentCell->next != NULL)
+    { //on se place à la fin de la liste
+      currentCell = currentCell->next;
     }
-  currentCell->next = newCell; //on chaine la nouvelle cellule
+    currentCell->next = newCell; //on chaine la nouvelle cellule
   }
 }
 }
@@ -30,7 +34,8 @@ void freeList(commandList *list)
 {
   commandListCell *head = list->head;
   commandListCell *tmp;
-  while (head->next != NULL){ //Tant que la liste n'est pas vide on supprime decale la tete vers l'avant et on supprime la cellule
+  while (head->next != NULL)
+  { //Tant que la liste n'est pas vide on supprime decale la tete vers l'avant et on supprime la cellule
     tmp = head;
     head = head->next;
     free(tmp);
@@ -38,21 +43,25 @@ void freeList(commandList *list)
   list->head = NULL; //une fois la liste vide on réinitialise la tete et la queue
 }
 
-cmdFunction find(commandList list, char* name)
+cmdFunction find(commandList list, char *name) //renvoie la fonction si elle est trouvée
 {
   commandListCell *currentFunction = list.head;
-  while (strcmp(name,currentFunction->function.name) && currentFunction->next !=NULL) { //tant que la fonction n'a pas été trouvée et qu'on est pas à la fin de la liste
+  while (strcmp(name, currentFunction->function.name) && currentFunction->next != NULL)
+  { //tant que la fonction n'a pas été trouvée et qu'on est pas à la fin de la liste
     currentFunction = currentFunction->next;
   }
-  if (!strcmp(name,currentFunction->function.name)) { //si la fonction a été trouvée
+  if (!strcmp(name, currentFunction->function.name))
+  {                                            //si la fonction a été trouvée
     return currentFunction->function.function; //retourne la commande
   }
-  else{
+  else
+  {
     return 0; //A modifier, il faudrait renvoyer un message comme quoi la fonction n'existe pas ou une commande d'erreur
   }
 }
 
-commandList initializeList(){
+commandList initializeList()
+{
   commandList list;
   list.head = NULL;
 
--- interpreteur/linkedList.h
+++ interpreteur/linkedList.h
@@ -1,32 +1,33 @@
 #ifndef LINKEDLIST_H
 #define LINKEDLIST_H
-
+#include <stdlib.h>
 #include "command.h"
 
 //Cellule d'une liste chainée
-typedef struct commandListCell{
+typedef struct commandListCell
+{
     cmdIdentifier function;
-    commandListCell* next;
-}commandListCell;
+    commandListCell *next;
+} commandListCell;
 
 //Liste chainée
-typedef struct commandList{
-    commandListCell* head;
-}commandList;
+typedef struct commandList
+{
+    commandListCell *head;
+} commandList;
 
 //ajoute un élément à une liste chainée
 //de préférence trié par ordre alphabétique
-void add(commandList *list,cmdIdentifier function);
+void add(commandList *list, cmdIdentifier function);
 
 //libère une liste chainée
 void freeList(commandList *list);
 
 //trouve et renvoit la fonction principale d'une commande nommée name
 //contenue dans une liste
-cmdFunction find(commandList list, char* name);
+cmdFunction find(commandList list, char *name);
 
 //Retourne une liste vide
 commandList initializeList();
-
 
 #endif
