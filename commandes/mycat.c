d9b94c10ecfb3c6537478c40c09d491947f957cb
Romain Andrieux <romandrieux29@gmail.com>
2018-04-11 16:32:58 +0200

    Commande cat(.c)

---
+++ commandes/mycat.c
@@ -0,0 +1,29 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include "mycat.h"
+#include <unistd.h>
+
+int main (int argc, char*argv[]){
+  int i;
+  char ligne[1000] = "";
+  FILE *fichier = NULL;
+
+  for (i=1; i<argc; i++){
+    fichier = fopen(argv[i],"r");
+
+    if(!fichier){
+      printf("This file does not exist, check the name\n");
+      break;
+    }
+
+    else{
+      while(fgets(ligne,1000,fichier)){
+        printf("%s",ligne);
+      }
+    }
+
+    fclose(fichier);
+  }
+
+  return(0);
+}
