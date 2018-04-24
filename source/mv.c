1d2978088a63503ac058f069fcb57ce6673060d2
Romain Andrieux <romain.andrieux1@etu.univ-nantes.fr>
2018-04-24 23:52:35 +0200

    MAJ cp mv

--- source/mv.c
+++ source/mv.c
@@ -1,35 +1,68 @@
 #include "mv.h"
-#include "./comCommunication.h"
 
 void mv(int argc, char *argv[])
 {
+  DIR *destination = NULL;
   FILE *source = NULL;
-  FILE *destination = NULL;
-  char chaine[1000] = "";
-  if (argc < 3)
-  {
-    printErr("Pas assez d'argument pour mv.\n");
-    return;
+  FILE *copieSource = NULL;
+
+  if(argc<2){
+    printErr("mv: opérande manquant\n");
   }
-  source = fopen(argv[1], "r");
-  destination = fopen(argv[2], "w");
-  if (source == NULL)
-  {
-    printErr("Le fichier source \"%s\" est introuvable.\n", argv[1]);
-    return;
-  }
-  while (fgets(chaine, 1000, source) != NULL)
-  {
-    fputs(chaine, destination);
-  }
-  fclose(source);
-  fclose(destination);
-  if (remove(argv[1]) == 0)
-  {
-    print("le fichier \"%s\" a bien été supprimé.\n", argv[1]);
-  }
-  else
-  {
-    printErr("Erreur lors de la suppression de %s.\n", argv[1]);
+  else{
+    destination = opendir(argv[argc-1]);
+
+    if(destination==NULL){ //si le dernier argument n'est pas un répertoire mais un fichier
+      if(argc>3){ //si on a plus de 2 noms de fichier en argument, alors on s'attend à déplacer les (n-1) premiers fichiers dans le n-ième argument qui doit être un réperoire. Or celui ci est un fichier.
+        printErr("La cible '%s' n'est pas un répertoire.\n",argv[argc-1]); //Donc on renvoie une erreur.
+        return;
+      }
+      else{ //si on a 2 noms de fichiers en argument, alors on renomme le 1er fichier avec le nom du 2ème fichier
+        source = fopen(argv[1], "r"); //on ouvre le fichier source
+        copieSource = fopen(argv[2], "w"); //on ouvre le fichier qui sera une copie de la source (s'il n'existe pas il sera créé automatiquement)
+        char chaine[1000] = "";
+
+        if (source == NULL) //si le fichier source n'existe pas
+        {
+          printErr("Le fichier source \"%s\" est introuvable.\n", argv[1]);
+          return;
+        }
+        while (fgets(chaine, 1000, source) != NULL) //copie ligne par ligne du fichier source
+        {
+          fputs(chaine, copieSource);
+        }
+        fclose(source); //on ferme le fichier source
+        fclose(copieSource); //on ferme la copie du fichier source
+        remove(argv[1]); //on supprime le fichier source
+      }
+    }
+    else{ //si le dernier argument est un répertoire
+      closedir(destination);
+      for(int i=1; i<argc-1; i++){ //on déplace les (n-1) fichiers dans le répertoire
+
+        char *dest = malloc(sizeof(char)*256);
+        strcpy(dest,argv[argc-1]);
+
+        strcat(dest,"/"); //on concatène le chemin de destination avec le séparateur '/' ...
+        strcat(dest,argv[i]); //...puis avec le nom du fichier source
+
+        copieSource = fopen(dest, "w"); //ainsi on crée une copie de la source dans le répertoire de destination
+        source = fopen(argv[i], "r");
+
+        char chaine[1000] = "";
+        if (source == NULL) //si le fichier à déplacer n'existe pas
+        {
+          printErr("Le fichier source \"%s\" est introuvable.", argv[i]);
+          return;
+        }
+        while (fgets(chaine, 1000, source) != NULL) //copie ligne par ligne du fichier source
+        {
+          fputs(chaine, copieSource);
+        }
+        fclose(source);
+        fclose(copieSource);
+        remove(argv[i]); //on supprime le fichier source
+      }
+    }
   }
 }
--- source/mv.h
+++ source/mv.h
@@ -2,6 +2,8 @@
 #define MV_H
 #include <stdio.h>
 #include <stdlib.h>
+#include <dirent.h>
+#include <string.h>
 #include "./comCommunication.h"
 void mv(int argc, char *argv[]);
 
