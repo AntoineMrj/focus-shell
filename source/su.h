b2bef16a514dcd9a78264101b1a4ef91bf61e984
Alexandre Nonnon <alexandre.nonnon@etu.univ-nantes.fr>
2018-04-24 23:07:18 +0200

    ajout de l'ébauche de su et du .h

---
+++ source/su.h
@@ -0,0 +1,11 @@
+#ifndef SU_H
+#define SU_H
+#include <stdlib.h>
+#include <stdio.h>
+#include "comCommunication.h"
+#include <sys/types.h>
+#include <unistd.h>
+
+void su(int argc, char *argv[]);
+
+#endif
