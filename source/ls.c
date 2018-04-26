bbf80ea12aa3149d493198f5d6f985153b010c81
Romain Andrieux <romain.andrieux1@etu.univ-nantes.fr>
2018-04-26 23:26:43 +0200

    Ajout options -a -l à ls

--- source/ls.c
+++ source/ls.c
@@ -1,31 +1,189 @@
 #include "ls.h"
 
-void ls(int argc, char *argv[])
+//Fonction pour ls-l (affiche en plus les informations du fichier en argument)
+void show_file_info(char *filename, struct stat *info_p)
 {
-  DIR *dirp;
+	char	*uid_to_name(), *ctime(), *gid_to_name(), *filemode();
+	void mode_to_letters();
+	char modestr[11];
+
+	mode_to_letters(info_p->st_mode, modestr);
+
+	print("%s %d %s %s %ld %.12s ", modestr, (int)info_p->st_nlink, uid_to_name(info_p->st_uid), gid_to_name(info_p->st_gid), (long)info_p->st_size, 4+ctime(&info_p->st_mtime));
+  print(GRN "%s\n" RESET , filename);
+}
+
+
+//Fonction pour convertir le mode (droits d'un fichier) en lettres
+void mode_to_letters(int mode, char str[])
+{
+	strcpy(str, "----------");
+
+	if(S_ISDIR(mode)) str[0] = 'd';		//répertoire
+	if(S_ISCHR(mode)) str[0] = 'c';		//périphérique de type caractère
+	if(S_ISBLK(mode)) str[0] = 'b';		//périphérique de type bloc
+
+	if(mode & S_IRUSR) str[1] = 'r';	//3 bits pour l'utilisateur
+	if(mode & S_IWUSR) str[2] = 'w';
+	if(mode & S_IXUSR) str[3] = 'x';
+
+	if(mode & S_IRGRP) str[4] = 'r';	//3 bits pour le groupe
+	if(mode & S_IWGRP) str[5] = 'w';
+	if(mode & S_IXGRP) str[6] = 'x';
+
+	if(mode & S_IROTH) str[7] = 'r';	//3 bits pour les autres
+	if(mode & S_IWOTH) str[8] = 'w';
+	if(mode & S_IXOTH) str[9] = 'x';
+
+}
+
+
+//Fonction qui renvoie un pointeur sur le nom de l'utilisateur associé à uid
+char *uid_to_name(uid_t uid)
+{
+	struct passwd *getpwuid(), *pw_ptr;
+	static char numstr[10];
+
+	if((pw_ptr = getpwuid(uid)) == NULL){
+		sprintf(numstr, "%d", uid);
+		return numstr;
+	}
+	else
+		return pw_ptr->pw_name;
+}
+
+//Fonction qui renvoie un pointeur sur le nom du groupe associé à gid
+char *gid_to_name(gid_t gid)
+{
+	struct group *getgrgid(), *grp_ptr;
+	static char numstr[10];
+
+	if((grp_ptr = getgrgid(gid)) == NULL){
+		sprintf(numstr, "%d", gid);
+		return numstr;
+	}
+	else
+		return grp_ptr->gr_name;
+}
+
+
+//Fonction pour parcourir un dossier
+void parcoursDossier(int a, int l, char *repertoire_courant){
+  int nb;
+  int i;
   struct dirent *dptr;
+  DIR *dirp;
+  struct stat s;
+
+  dirp = opendir(repertoire_courant);
+  //Détermination du nombre l de ficheirs dans le dossier courant:
+  nb=0;
+  while ((dptr = readdir(dirp)))
+  {
+    if(a==1)
+      nb++;
+    else{
+      if (dptr->d_name[0] != '.'){
+        nb++;
+      }
+    }
+  }
+  closedir(dirp);
+
+  i=0;
+  dirp = opendir(repertoire_courant);
+  while ((dptr = readdir(dirp)))
+  {
+    if(a==1){
+      i++;
+      if(l){
+        stat(dptr->d_name,&s);
+        show_file_info(dptr->d_name,&s);
+      }
+      else if(i==nb)
+        print(GRN "%s\n" RESET, dptr->d_name);
+      else
+        print(GRN "%s " RESET, dptr->d_name);
+    }
+    else{
+      if (dptr->d_name[0] != '.'){
+        i++;
+        if(l){
+          stat(dptr->d_name,&s);
+          show_file_info(dptr->d_name,&s);
+        }
+        else if(i==nb)
+          print(GRN "%s\n" RESET, dptr->d_name);
+        else
+          print(GRN "%s " RESET, dptr->d_name);
+      }
+    }
+  }
+  closedir(dirp);
+}
+
+//Fonction principale de ls
+void ls(int argc, char *argv[]){
+  DIR *dirp;
   char *repertoire_courant = malloc(sizeof(char) * 256);
   size_t size = 256;
-  if (argc == 1)
+  int a=0;
+  int l=0;
+  char optstring[]="al";
+  int c;
+  struct stat s;
+  int argc2 = argc;
+
+  while((c=getopt(argc, argv, optstring)) != -1){
+    switch(c){
+      case 'a':
+            a=1;
+            argc2--;
+            break;
+      case 'l':
+            l=1;
+            argc2--;
+            break;
+      case '?':
+            printErr("Ligne de commande incorrecte.\n");
+            return;
+    }
+  }
+
+  if (argc2 == 1)
   {
     getcwd(repertoire_courant, size);
+    parcoursDossier(a, l, repertoire_courant);
   }
   else
   {
-    repertoire_courant = argv[1];
-  }
-  if ((dirp = opendir(repertoire_courant)) == NULL)
-  {
-    printErr("Erreur le dossier %s n'existe pas.\n", argv[1]);
-  }
-  else
-  {
-
-    while ((dptr = readdir(dirp)))
-    {
-      if (dptr->d_name[0] != '.')
-        print("%s\n", dptr->d_name);
+    for(int i=1; i<argc; i++){
+      if(argv[i][0]!='-'){
+        repertoire_courant = argv[i];
+        if ((dirp = opendir(repertoire_courant)) == NULL) //si l'argument n'est pas un repertoire mais un fichier
+        {
+          if(i+1!=argc){
+            if(l){
+              stat(argv[i],&s);
+              show_file_info(argv[i],&s);
+            }
+            else
+              print(GRN "%s " RESET,argv[i]); //on affiche le nom du fichier
+          }
+          else{
+            if(l){
+              stat(argv[i],&s);
+              show_file_info(argv[i],&s);
+            }
+            else
+              print(GRN "%s\n" RESET,argv[i]);
+          }
+        }
+        else //si l'argument est un dossier
+        {
+          parcoursDossier(a, l, repertoire_courant);
+        }
+      }
     }
-    closedir(dirp);
   }
 }
--- source/ls.h
+++ source/ls.h
@@ -4,7 +4,19 @@
 #include <stdlib.h>
 #include <dirent.h>
 #include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <pwd.h>
+#include <grp.h>
+#include <string.h>
 #include "comCommunication.h"
+#include "printColor.h"
+
 void ls(int argc, char *argv[]);
+void parcoursDossier(int a, int l, char *repertoire_courant);
+void show_file_info(char *, struct stat *);
+void mode_to_letters(int, char[]);
+char *uid_to_name(uid_t);
+char *gid_to_name(gid_t);
 
 #endif
